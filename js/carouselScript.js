c0ea41044106d0ae9b2eb1de1c729e372c3100e4
Mina Mazloomi <Minamazloomy@gmail.com>
2021-12-05 23:57:53 -0800

    Add files via upload
    I cloned the main branch and added the changes on master branch to it.
    My Changes are:
     -Changing carousel images every 4 seconds (carouselScript.js)
    -Text added to the logo (index.html ,mainStyle.css)
    -Navigation bar background-color bug fixed(toggleMenu.js)
    -In the Mobile mode the position of logo-box and navigation button changed (mainStyle.css)
    -In the Mobile mode size of page header changed (mainStyle.css)
    -romance.html file edited
    -aventure.html , volunteer.html and cruise.html added

--- js/carouselScript.js
+++ js/carouselScript.js
@@ -1,5 +1,7 @@
+
 const topContainer = document.getElementById('topContainer');
-const pageTitle = document.getElementById('pageTitle');
+const pageTitleDesktop = document.getElementById('pageTitleDesktop');
+const pageTitleMobile = document.getElementById('pageTitleMobile');
 const slides = document.querySelectorAll('.slide');
 const leftBtn= document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
@@ -34,8 +36,8 @@ setBgToBody();
 
 function setBgToBody(){
     topContainer.style.backgroundImage = slides[activeSlide].style.backgroundImage;
-    pageTitle.textContent= slides[activeSlide].title;
-
+    pageTitleDesktop.textContent= slides[activeSlide].title;
+    pageTitleMobile.textContent= slides[activeSlide].title;
 }
 
 function setActiveSlide(){
@@ -43,3 +45,69 @@ function setActiveSlide(){
 
     slides[activeSlide].classList.add('active');
 }
+// changing images on carousel every 4 seconds
+let interval = setInterval( run , 4000);
+
+function run(){
+    activeSlide--;
+
+    if(activeSlide < 0){
+        activeSlide = slides.length-1;
+    }
+
+    setBgToBody();
+    setActiveSlide();
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    interval = setInterval(run,4000)
+}
+
+
+
+// const topContainer = document.getElementById('topContainer');
+// const pageTitle = document.getElementById('pageTitle');
+// const slides = document.querySelectorAll('.slide');
+// const leftBtn= document.getElementById('leftBtn');
+// const rightBtn = document.getElementById('rightBtn');
+
+// let activeSlide = 0 ;
+
+// rightBtn.addEventListener('click', ()=>{
+//     activeSlide++;
+
+//     if(activeSlide > slides.length-1){
+//         activeSlide = 0;
+//     }
+
+//     setBgToBody();
+//     setActiveSlide();
+//  })
+
+// leftBtn.addEventListener('click', ()=>{
+//     activeSlide--;
+
+//     if(activeSlide < 0){
+//         activeSlide = slides.length-1;
+//     }
+
+//     setBgToBody();
+//     setActiveSlide();
+// })
+
+
+
+// setBgToBody();
+
+// function setBgToBody(){
+//     topContainer.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+//     pageTitle.textContent= slides[activeSlide].title;
+
+// }
+
+// function setActiveSlide(){
+//     slides.forEach(slide => slide.classList.remove('active'));
+
+//     slides[activeSlide].classList.add('active');
+// }
